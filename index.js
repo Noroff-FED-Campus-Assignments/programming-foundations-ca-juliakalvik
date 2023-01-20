74effb2caacf1a5e14275d130e892ddfc2cfa0c5
juliakalvik <julia.kalvik@yahoo.no>
2023-01-20 15:14:51 +0100

    All buttons working

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var heading = document.querySelector("h2");
 var button = document.querySelector("button");
 var buttonLower = document.querySelector(".lower");
+var heading2 = document.querySelector(".h2shutter");
+var buttonShutterUp = document.querySelector(".shutter_up")
+var buttonShutterDown = document.querySelector(".shutter_down")
+var heading3 = document.querySelector(".h2apt");
+var buttonApertureUp = document.querySelector(".aperture_up")
+var buttonApertureDown = document.querySelector(".aperture_down")
+
 
 const owner = {
     firstName: "Julia",
@@ -41,19 +48,42 @@ const owner = {
         ]
     };
   
-  const camera = [cameraInfo, cameraSettings];
+  const camera = [cameraInfo, cameraSettings, cameraSettingsIso];
   
   console.log(camera[1])
 
 function changeIsoHigher() {
-    heading.innerHTML = "ISO 200" ;
+    heading.innerHTML = "ISO: 200" ;
 }
 
   button.onclick = changeIsoHigher;
 
   function changeIsoLower() {
-    heading.innerHTML = "ISO 100";
+    heading.innerHTML = "ISO: 100";
   }
 
   buttonLower.onclick = changeIsoLower;
 
+  function changeShutterUp() {
+    heading2.innerHTML = "Shutter speed: 380";
+  }
+
+  buttonShutterUp.onclick = changeShutterUp;
+
+  function changeShutterDown() {
+    heading2.innerHTML = "Shutter speed: 250";
+  }
+
+  buttonShutterDown.onclick = changeShutterDown;
+
+  function changeApertureUp() {
+    heading3.innerHTML = "Aperture: 2.8";
+  }
+
+  buttonApertureUp.onclick = changeApertureUp;
+
+  function changeApertureDown() {
+    heading3.innerHTML = "Aperture: 1.4";
+  }
+
+  buttonApertureDown.onclick = changeApertureDown;
